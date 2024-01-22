7e2305f58dded76d7a8b86e782c72b51d31bda45
patportugal <157312773+patportugal@users.noreply.github.com>
2024-01-22 10:41:19 +0100

    js and css working

--- app/script.js
+++ app/script.js
@@ -1,33 +1,3 @@
-let currentTime = new Date();
-let dayTime = document.querySelector("#time");
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let week = days[currentTime.getDay()];
-
-let hour = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-
-dayTime.innerHTML = week + " " + hour + ":" + minutes;
-
-let city = document.querySelector("h1");
-let city2 = document.querySelector("#searching");
-
-city2.addEventListener("submit", local);
-
-function local(event) {
-  event.preventDefault();
-  let cidade = document.querySelector("#city").value;
-  city.innerHTML = cidade;
-}
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
@@ -49,3 +19,21 @@ function search(event) {
 
   axios.get(apiUrl).then(displayTemperature);
 }
+
+let currentTime = new Date();
+let dayTime = document.querySelector("#time");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let week = days[currentTime.getDay()];
+let hour = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+
+dayTime.innerHTML = week + " " + hour + ":" + minutes;
