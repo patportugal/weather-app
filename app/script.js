6572b53ee650b6aaa256ae0e1b68fce5331d28b9
patportugal <157312773+patportugal@users.noreply.github.com>
2024-01-22 10:37:13 +0100

    search-engine added

--- app/script.js
+++ app/script.js
@@ -27,3 +27,25 @@ function local(event) {
   let cidade = document.querySelector("#city").value;
   city.innerHTML = cidade;
 }
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = `753ef61a4c9704b0boa8ce19973atca6`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=753ef61a4c9704b0boa8ce19973atca6&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
